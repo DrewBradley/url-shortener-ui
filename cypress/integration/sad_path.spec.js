88945ba3d561171421ada97d6156273ed5f3dbee
Drew Bradley <williamandrewb@gmail.com>
2021-03-09 11:57:34 -0700

    Start writing sad path testing, realize it's almost noon, submit assessment instead.

---
+++ cypress/integration/sad_path.spec.js
@@ -0,0 +1,9 @@
+context('SAD path', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should send by an error if the request fails', () => {
+    // cy.get('div')
+  })
+})
